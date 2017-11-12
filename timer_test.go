0b8e2799c9ef55a218a59e2fabf987c393ddca51
singchia <singchia@163.com>
2017-11-12 22:18:26 +0800

    add test

--- timer_test.go
+++ timer_test.go
@@ -28,3 +28,30 @@ func Test_Stop(t *testing.T) {
 	t.Log(err.Error())
 	return
 }
+
+func Test_Pause(t *testing.T) {
+	tw := NewTimer()
+	tw.Start()
+	tick, _ := tw.Time(2, time.Now(), nil, nil)
+	tw.Pause()
+	select {
+	case <-tick.Tunnel():
+		t.Error("not paused")
+	case <-time.NewTimer(time.Second * 5).C:
+		t.Log("paused and timeout")
+	}
+	return
+}
+
+func Test_Moveon(t *testing.T) {
+	tw := NewTimer()
+	tw.Start()
+	tick, _ := tw.Time(2, time.Now(), nil, nil)
+	tw.Pause()
+	time.Sleep(time.Second)
+	tw.Moveon()
+	o := <-tick.Tunnel()
+	elapse := time.Since(o.(time.Time))
+	t.Log(elapse)
+	return
+}
